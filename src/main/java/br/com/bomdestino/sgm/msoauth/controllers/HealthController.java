e1a2ae9db74ea676afc4d4086d6acdd5e2aa80f4
João Pedro Amaral Souza <joaopedroa.6@hotmail.com>
2021-04-06 19:28:15 -0300

    Incluindo health

---
+++ src/main/java/br/com/bomdestino/sgm/msoauth/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package br.com.bomdestino.sgm.msoauth.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/health")
+	public ResponseEntity<String> getHealth(){
+		return new ResponseEntity<String>("up", HttpStatus.OK);
+	}
+}
