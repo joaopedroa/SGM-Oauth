92729d9758cd890c11113b9b7cb1c345b4b8f2f2
João Pedro Amaral Souza <joaopedroa.6@hotmail.com>
2021-04-06 20:13:04 -0300

    Configurando liberar health

--- src/main/java/br/com/bomdestino/sgm/msoauth/configs/SecurityConfig.java
+++ src/main/java/br/com/bomdestino/sgm/msoauth/configs/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	 @Override
 	 protected void configure(HttpSecurity http) throws Exception {
-	   http.authorizeRequests()
+	   http.csrf().disable().authorizeRequests()
 	   .antMatchers("/api/v1/health").permitAll() 
 	   .anyRequest().authenticated();
 	 }
