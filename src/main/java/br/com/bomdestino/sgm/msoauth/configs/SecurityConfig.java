11239cc2a141e11ca9edd59287ba3488abd802f6
João Pedro Amaral Souza <joaopedroa.6@hotmail.com>
2021-04-06 20:05:15 -0300

    Liberando rota health

--- src/main/java/br/com/bomdestino/sgm/msoauth/configs/SecurityConfig.java
+++ src/main/java/br/com/bomdestino/sgm/msoauth/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,4 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
+	
+	 @Override
+	 protected void configure(HttpSecurity http) throws Exception {
+	   http.authorizeRequests()
+	   .antMatchers("/api/v1/health").permitAll() 
+	   .anyRequest().authenticated();
+	 }
 }
